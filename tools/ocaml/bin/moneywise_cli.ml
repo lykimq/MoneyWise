8e8ff711af7bd769864d4b6eb16008dd1cf32f94
lykimq <lykimq@gmail.com>
2025-08-30 13:33:24 +0200

    Tools: fix the loging level display

--- tools/ocaml/bin/moneywise_cli.ml
+++ tools/ocaml/bin/moneywise_cli.ml
@@ -3,6 +3,16 @@
 open Cmdliner
 open Commands
 
+(** Set up logging reporter to display logs *)
+let setup_logging () =
+  (* Initialize Fmt_tty for proper terminal output *)
+  Fmt_tty.setup_std_outputs ();
+  (* Set up a basic console reporter that shows all log levels *)
+  let reporter = Logs.format_reporter () in
+  Logs.set_reporter reporter;
+  (* Set default log level to info *)
+  Logs.set_level (Some Logs.Info)
+
 (** Main command group *)
 let cmds = [ verify_cmd; test_cmd ]
 
@@ -13,4 +23,6 @@ let main_cmd =
   let info = Cmd.info "moneywise" ~version:"1.0.0" ~doc in
   Cmd.group info cmds
 
-let () = exit (Cmd.eval main_cmd)
+let () =
+  setup_logging ();
+  exit (Cmd.eval main_cmd)
